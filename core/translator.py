ce275f6dccddd36cc1d742c87a5cd069395161fb
mrf0rtuna4 <majned90@gmail.com>
2024-05-02 16:37:06 +0300

    вывертыш

--- core/translator.py
+++ core/translator.py
@@ -1,23 +1,26 @@
 from deep_translator import GoogleTranslator
 import re
-import time
+import os
 
 def read_readme():
     with open("README.md", "r", encoding="utf-8") as file:
         return file.read()
 
+
 def update_localizations():
+    langs = GoogleTranslator().get_supported_languages()
     readme_content = read_readme()
+    selected_langs = os.getenv("LANGS")
     no_links_content = re.sub(r"\[([^\]]+)\]\([^\)]+\)", r"\1", readme_content)
-    src_lang = GoogleTranslator(source='auto', target='en').detect(text=no_links_content)
 
     languages = ['ru', 'fr', 'es']
     for lang in languages:
-        if lang != src_lang:
-            translated_content = GoogleTranslator(source=src_lang, target=lang).translate(text=no_links_content)
+        translated_content = GoogleTranslator(
+            source='auto', target=lang).translate(text=no_links_content)
 
-            with open(f"{lang}.md", "w", encoding="utf-8") as file:
-                file.write(translated_content)
-            print(f"Localization for {lang} updated.")
+        with open(f"{lang}.md", "w", encoding="utf-8") as file:
+            file.write(translated_content)
+        print(f"Localization for {lang} updated.")
+
 
 update_localizations()
