d0e543b39d07e741e5b5b164c988ce4e7aa9125a
mrf0rtuna4 <majned90@gmail.com>
2024-05-02 15:47:11 +0300

    задержка (в развитии)

--- core/translator.py
+++ core/translator.py
@@ -1,5 +1,6 @@
 import re
-from googletrans import Translator, LANGUAGES
+from googletrans import Translator
+import time
 
 def read_readme():
     with open("README.md", "r", encoding="utf-8") as file:
@@ -11,15 +12,17 @@ def update_localizations():
 
     no_links_content = re.sub(r"\[([^\]]+)\]\([^\)]+\)", r"\1", readme_content)
 
-    src_lang = translator.detect(no_links_content).lang
+    src_lang = None
+    while src_lang is None:
+        try:
+            src_lang = translator.detect(no_links_content).lang
+        except AttributeError:
+            time.sleep(1)
 
-    languages = ['ru', 'fr', 'es']  
+    languages = ['ru', 'fr', 'es']
     for lang in languages:
         if lang != src_lang:
             translated_content = translator.translate(no_links_content, src=src_lang, dest=lang).text
-
-            translated_content = re.sub(r"exclude_from_translation: true", "", translated_content)
-
             with open(f"locales/{lang}.md", "w", encoding="utf-8") as file:
                 file.write(translated_content)
             print(f"Localization for {lang} updated.")
