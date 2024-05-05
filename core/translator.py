476a240879dc6627ddd8cf0c0fe19123710f253f
mrf0rtuna4 <majned90@gmail.com>
2024-05-05 19:58:54 +0300

    renames

--- core/translator.py
+++ core/translator.py
@@ -9,15 +9,15 @@ async def decompile_readme():
         readme_content = file.read()
 
     code_blocks = re.findall(r"```[\s\S]*?```", readme_content)
-    non_code_blocks = re.sub(r"```[\s\S]*?```", "6043610", readme_content)
-    links = re.findall(r"\[([^]]+)]\(([^)]+)\)", non_code_blocks)
-    non_code_blocks = re.sub(r"\[([^]]+)]\(([^)]+)\)", "1146", non_code_blocks)
-    html_tags = re.findall(r"<.*?>", non_code_blocks)
-    non_code_blocks = re.sub(r"<.*?>", "5121726", non_code_blocks)
+    supported_content = re.sub(r"```[\s\S]*?```", "10001", readme_content)
+    links = re.findall(r"\[([^]]+)]\(([^)]+)\)", supported_content)
+    supported_content = re.sub(r"\[([^]]+)]\(([^)]+)\)", "10002", supported_content)
+    html_tags = re.findall(r"<.*?>", supported_content)
+    supported_content = re.sub(r"<.*?>", "10003", supported_content)
 
     chunk_size = 5000
-    chunks = [non_code_blocks[i:i + chunk_size]
-              for i in range(0, len(non_code_blocks), chunk_size)]
+    chunks = [supported_content[i:i + chunk_size]
+              for i in range(0, len(supported_content), chunk_size)]
 
     return chunks, {"code_blocks": code_blocks, "links": links, "html_tags": html_tags}
 
@@ -26,13 +26,13 @@ async def build_readme(translated_chunks, data):
     translated_content = " ".join(translated_chunks)
 
     for i, code_block in enumerate(data["code_blocks"]):
-        translated_content = translated_content.replace(f"6043610", code_block, 1)
+        translated_content = translated_content.replace(f"10001", code_block, 1)
 
     for i, link in enumerate(data["links"]):
-        translated_content = translated_content.replace(f"1146", f"[{link[0]}]({link[1]})", 1)
+        translated_content = translated_content.replace(f"10002", f"[{link[0]}]({link[1]})", 1)
 
     for i, html_tag in enumerate(data["html_tags"]):
-        translated_content = translated_content.replace(f"5121726", html_tag, 1)
+        translated_content = translated_content.replace(f"10003", html_tag, 1)
 
     return translated_content
 
