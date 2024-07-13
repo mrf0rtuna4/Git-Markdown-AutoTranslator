6987c49a7d987004889e4d04a19fb36d63b60f50
mrf0rtuna4 <majned90@gmail.com>
2024-07-13 13:02:49 +0300

    fix!: init

---
+++ core/app/__init__.py
@@ -0,0 +1,4 @@
+from .localization_manager import LocalizationManager
+from .logger import log_error, log_info
+from .markdown_processor import MarkdownProcessor
+from .readme_handler import ReadmeHandler
